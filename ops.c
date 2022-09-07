9d5793531e5c1d2ea555a6c07b9ecf199ada18ac
Maxwell D. Dorliea <deboy170@gmail.com>
2022-09-07 11:53:09 +0000

    Fixed bug in task 2

--- ops.c
+++ ops.c
@@ -123,7 +123,7 @@ void pop(stack_t **top, unsigned int line_number)
 
 	if (!top || !(*top))
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack,\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
